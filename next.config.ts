78f31528fe7e0f3601f462401e460963b5611450
Syihabudin Tsani <syihabudintsani15@gmail.com>
2025-09-18 22:23:47 +0700

    [improve] next config

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // /* config options here */
-  // // webpack: (config, { isServer }) => {
-  // //   if (!isServer) {
-  // //     config.resolve.fallback = {
-  // //       ...config.resolve.fallback,
-  // //       canvas: false,
-  // //     };
-  // //   }
-  // //   return config;
-  // // },
-  // // target: "serverless",
-  // future: { webpack5: true },
-  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-  //   config.resolve.alias.canvas = false;
-  //   config.resolve.alias.encoding = false;
-  //   return config;
-  // },
+  /* config options here */
+  future: { webpack5: true },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
 };
 
 export default nextConfig;
